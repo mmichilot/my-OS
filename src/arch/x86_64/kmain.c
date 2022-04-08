f48e72b6bf67cdc7da4973b095a111d18527affb
Matthew Michilot <matthew.michilot@gmail.com>
2022-04-08 14:36:12 -0700

    Uses vga driver

--- src/arch/x86_64/kmain.c
+++ src/arch/x86_64/kmain.c
@@ -1,7 +1,22 @@
-void kmain() {
+#include "vga.h"
 
+void kmain() 
+{
+
+#ifdef DEBUG
+    int loop = 1;
+    while(loop);
+#endif
+ 
+    VGA_init();
+    VGA_str("Hello, World!\n");
+    VGA_bg_color(LIGHT_GRAY);
+    VGA_str("Hello, World!\n");
+    VGA_bg_color(PINK);
+    VGA_str("Hello, World\n");
+    
     // Halt indefinitely
     while (1) {
-        __asm__("hlt");
+        asm volatile ("hlt");
     }
 }
